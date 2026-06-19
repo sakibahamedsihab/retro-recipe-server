6a4167e73ada3e30f0512237d184a4787a283e8e
sakibahamedsihab <sakiba505@gmail.com>
2026-06-19 15:42:15 +0600

    changed samSite -> 'none'

--- routes/auth.js
+++ routes/auth.js
@@ -49,7 +49,7 @@ router.post("/jwt", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
